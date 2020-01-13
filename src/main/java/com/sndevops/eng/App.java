73a475a9c9e99beb5f500ecea200e052aa6fc7f4
rrasane <47263869+rrasane@users.noreply.github.com>
2020-01-13 15:28:29 -0800

    Fixed bug

--- src/main/java/com/sndevops/eng/App.java
+++ src/main/java/com/sndevops/eng/App.java
@@ -12,9 +12,6 @@ public class App
     public static void main( String[] args )
         // test line
     {
-        int a = "1"; // buggy line
-        int b = "1"; // buggy line
-        int c = "1"; // buggy line
         System.out.println( "Hello devops again" );
     }
     // Comment for testing
