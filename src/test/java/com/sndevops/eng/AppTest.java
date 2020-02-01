2204a3faa0c62edf369366e066d93de6beacb514
rrasane <47263869+rrasane@users.noreply.github.com>
2020-01-31 16:04:04 -0800

    Update AppTest.java

--- src/test/java/com/sndevops/eng/AppTest.java
+++ src/test/java/com/sndevops/eng/AppTest.java
@@ -9,6 +9,7 @@ public class AppTest {
     // Third test
     // Test on 1/25/2020
     // tested on 1/31/2020
+    // tested on 4:03PM
     private String INPUT = "13411";
     @Test 
     public void testLength() { 
