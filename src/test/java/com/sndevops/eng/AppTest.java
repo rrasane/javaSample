0145acd68f759c2e5ae382f0401936272c7baebf
rrasane <47263869+rrasane@users.noreply.github.com>
2020-02-14 13:12:29 -0800

    Update AppTest.java

--- src/test/java/com/sndevops/eng/AppTest.java
+++ src/test/java/com/sndevops/eng/AppTest.java
@@ -6,6 +6,7 @@ public class AppTest {
     private String INPUT = "13411";
     @Test 
     public void testLength() { 
+        // committed on 2/14/2020
         Assert.assertEquals(INPUT,"13411");  
     }
 }
