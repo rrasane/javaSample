63153862e4d66340cf4c44265ef488b2a04d285d
rrasane <47263869+rrasane@users.noreply.github.com>
2020-02-14 13:06:17 -0800

    Update AppTest.java

--- src/test/java/com/sndevops/eng/AppTest.java
+++ src/test/java/com/sndevops/eng/AppTest.java
@@ -3,7 +3,6 @@ import org.junit.Assert;
 import org.junit.Test; 
 
 public class AppTest {
-    // committed on 2/14/2020
     private String INPUT = "13411";
     @Test 
     public void testLength() { 
